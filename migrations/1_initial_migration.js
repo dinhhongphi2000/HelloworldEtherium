d3eb5dbf0e368886ab0d9bc7f66bd0fd8e5a3247
Đinh Hồng Phi <dinhhongphi@gmail.com>
2018-04-02 21:53:17 +0700

    master branch

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var HelloEthSalon = artifacts.require("./HelloEthSalon.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(HelloEthSalon);
-};
